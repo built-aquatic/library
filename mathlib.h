64f25f72386253a34b1dbcb917b7f94960429c59
Ingrid Jansen <153064746+built-aquatic@users.noreply.github.com>
2023-12-06 14:47:56 +0700

    Create mathlib.h

---
+++ mathlib.h
@@ -0,0 +1,10 @@
+// mathlib.h
+#pragma once
+
+class MathLib {
+public:
+    static int add(int a, int b);
+    static int subtract(int a, int b);
+    static int multiply(int a, int b);
+    static double divide(int a, int b);
+};
